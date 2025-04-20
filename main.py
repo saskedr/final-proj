442f70d4659333365cc056760bb3b41530e98a08
Alexander Deryabin <alvderyabin.dev@gmail.com>
2025-04-20 13:21:49 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+import telebot
+    
+# Инициализация бота с использованием его токена
+bot = telebot.TeleBot("6296556273:AAE_YvL1d10wj4qU3L8DyptsUCiWuCiUdbk")
+
+# Обработчик команды '/start' и '/hello'
+@bot.message_handler(commands=['start', 'hello'])
+def send_welcome(message):
+    bot.reply_to(message, f'Привет! Я бот {bot.get_me().first_name}!')
+
+# Обработчик команды '/heh'
+@bot.message_handler(commands=['heh'])
+def send_heh(message):
+    count_heh = int(message.text.split()[1]) if len(message.text.split()) > 1 else 5
+    bot.reply_to(message, "he" * count_heh)
+
+# Запуск бота
+bot.polling()
